975d50c66db33dab073059e0efee1ab35bde72af
zhuchao <zhuchao@zhuchaodeMacBook-Pro.local>
2022-04-13 21:20:29 -0700

    提交代码

---
+++ priceserver/src/main/java/org/tron/priceserver/TestService.java
@@ -0,0 +1,10 @@
+package org.tron.priceserver;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+    public String test() {
+        return "test";
+    }
+}
